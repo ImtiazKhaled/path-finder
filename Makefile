647d0ee4a97fc059d461c5e9515e646e56a28014
Imtiaz <imtiazkhaled07@gmail.com>
2020-02-02 00:32:37 -0600

    implemented A* search

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ all:
 	./find_route input1.txt Bremen Kassel
 	./find_route input1.txt Karlsruhe Duesseldorf
 	./find_route input1.txt London Kassel
+	./find_route input1.txt Saarbruecken Kassel h_kassel.txt
+	./find_route input1.txt Bremen Kassel h_kassel.txt
--- find_route.cpp
+++ find_route.cpp
@@ -9,7 +9,6 @@
 
 // Tree Node Class
 class TreeNode {
-
     public:
         std::string start;
         std::unordered_map<TreeNode*, int> children;
@@ -22,7 +21,18 @@ class TreeNode {
         void AddChild(TreeNode* _destination, int _distance) {
             children[_destination] = _distance;
         }
+};
 
+class PathState {
+    public:
+        int total_distance;
+        TreeNode* last_node;
+
+        PathState(TreeNode* last_node, int total_distance) : last_node(last_node), total_distance(total_distance) {}
+
+        friend bool operator<(const PathState& p1, const PathState& p2) {
+            return p1.total_distance < p2.total_distance;
+        }
 };
 
 // Calculates First Uninformed Path To The Destination Node
@@ -32,7 +42,7 @@ void GetUninformedPath(TreeNode* root, std::string destination) {
     std::unordered_map<std::string, int> visited;
     std::vector<std::string> paths;
     queue.push(root);
-    TreeNode* CurrNode = NULL;
+    TreeNode* curr_node = NULL;
     int path = 0, max_nodes = 0, nodes_expanded = 0, nodes_generated = 0;
     bool flag = false;
     ++visited[root->start];
@@ -41,16 +51,16 @@ void GetUninformedPath(TreeNode* root, std::string destination) {
     while(!queue.empty() && !flag) {
         
         max_nodes = max(queue.size(), max_nodes);
-        CurrNode = queue.front();
+        curr_node = queue.front();
         queue.pop();
         ++nodes_expanded;
 
-        if(CurrNode->start == destination)  flag = true;
+        if(curr_node->start == destination)  flag = true;
 
-        for(auto child : CurrNode->children) {
+        for(auto child : curr_node->children) {
             if(visited[child.first->start] == 0) {
                 ++nodes_generated;
-                child.first->prev_node = CurrNode;
+                child.first->prev_node = curr_node;
                 queue.push(child.first);
                 ++visited[child.first->start];
             }
@@ -58,14 +68,77 @@ void GetUninformedPath(TreeNode* root, std::string destination) {
 
     }
 
-    while(CurrNode->prev_node !=  NULL && flag) {
+    while(curr_node->prev_node !=  NULL && flag) {
         std::ostringstream path_stream;
-        path_stream << CurrNode->prev_node->start << " to "<< CurrNode->start
-                    << ", " << CurrNode->prev_node->children[CurrNode] << ".0 km\n";
+        path_stream << curr_node->prev_node->start << " to "<< curr_node->start
+                    << ", " << curr_node->prev_node->children[curr_node] << ".0 km\n";
         std::string curr_path = path_stream.str();
         paths.push_back(curr_path);
-        path += CurrNode->prev_node->children[CurrNode]; 
-        CurrNode = CurrNode->prev_node;
+        path += curr_node->prev_node->children[curr_node]; 
+        curr_node = curr_node->prev_node;
+    }
+
+    std::cout << "nodes expanded: " << nodes_expanded << "\n"
+              << "nodes generated: " << nodes_generated << "\n"
+              << "max nodes in memory: " << max_nodes << "\n"
+              << "distance: " << path << "\n"
+              << "route: " << "\n";
+              
+    if(!flag) {
+        std::cout << "none" << std::endl;
+    } else {
+        for(int i = paths.size() - 1; i >= 0; --i) {
+            std::cout << paths[i];
+        }
+    }
+}
+
+
+// Calculates Optimal Path To The Destination Node By Checking Results With Heuristic 
+void GetInformedPath(TreeNode* root, std::string destination, 
+std::unordered_map<std::string, int> heuristic_map) {
+
+    std::priority_queue<PathState*> queue;
+    std::unordered_map<std::string, int> visited;
+    std::vector<std::string> paths;
+    PathState* root_state = new PathState(root,0);
+    PathState* curr_state = NULL;
+    queue.push(root_state);
+    int path = 0, max_nodes = 0, nodes_expanded = 0, nodes_generated = 0;
+    bool flag = false;
+    ++visited[root->start];
+
+    // BFS visited
+    while(!queue.empty() && !flag) {
+        
+        max_nodes = max(queue.size(), max_nodes);
+        curr_state = queue.top();
+        queue.pop();
+        ++nodes_expanded;
+
+        if(curr_state->last_node->start == destination)  flag = true;
+
+        for(auto child : curr_state->last_node->children) {
+            if(visited[child.first->start] == 0 && 
+            heuristic_map[curr_state->last_node->start] > heuristic_map[child.first->start]) {
+                ++nodes_generated;
+                child.first->prev_node = curr_state->last_node;
+                PathState* new_state = new PathState(child.first, curr_state->total_distance + child.second);
+                queue.push(new_state);
+                ++visited[child.first->start];
+            }
+        }
+    }
+
+    TreeNode* curr_node = curr_state->last_node;
+    while(curr_node->prev_node !=  NULL && flag) {
+        std::ostringstream path_stream;
+        path_stream << curr_node->prev_node->start << " to "<< curr_node->start
+                    << ", " << curr_node->prev_node->children[curr_node] << ".0 km\n";
+        std::string curr_path = path_stream.str();
+        paths.push_back(curr_path);
+        path += curr_node->prev_node->children[curr_node]; 
+        curr_node = curr_node->prev_node;
     }
 
     std::cout << "nodes expanded: " << nodes_expanded << "\n"
@@ -88,42 +161,63 @@ int main(int argc, char** argv) {
     
     int distance;
     std::string start, dest;
-    std::ifstream myfile (argv[1]);
     std::unordered_map<std::string, TreeNode*> in_map;
-    TreeNode* StartNode = NULL;
-    
+    std::unordered_map<std::string, int> heuristic_map;
+    TreeNode* start_node = NULL;
+    std::ifstream tree_source (argv[1]);
+
     // construct a Tree based on input file 
-    if (myfile.is_open())
-    {
-        while ( myfile >> start >> dest >> distance ) {
-            TreeNode* NodeStart = in_map[start];
-            TreeNode* NodeDest = in_map[dest];
+    if (tree_source.is_open()) {
+        while ( tree_source >> start >> dest >> distance ) {
+            TreeNode* node_start = in_map[start];
+            TreeNode* node_dest = in_map[dest];
             
             if(in_map[dest] == 0) {
-                NodeDest = new TreeNode(dest);
-                in_map[dest] = NodeDest;
+                node_dest = new TreeNode(dest);
+                in_map[dest] = node_dest;
             }
             
             if(in_map[start] == 0) {
-                NodeStart = new TreeNode(start);
-                in_map[start] = NodeStart;
+                node_start = new TreeNode(start);
+                in_map[start] = node_start;
             }
             
-            if(argv[2] == start && StartNode == NULL) {
-                StartNode = NodeStart;
-            } else if(argv[2] == dest && StartNode == NULL) {
-                StartNode = NodeDest;
+            if(argv[2] == start && start_node == NULL) {
+                start_node = node_start;
+            } else if(argv[2] == dest && start_node == NULL) {
+                start_node = node_dest;
             }
 
-            NodeStart->AddChild(NodeDest, distance);
-            NodeDest->AddChild(NodeStart, distance);
+            node_start->AddChild(node_dest, distance);
+            node_dest->AddChild(node_start, distance);
         }
 
-        myfile.close();
+        tree_source.close();
     }
 
     else std::cout << "Unable to open file"; 
     
-    GetUninformedPath(StartNode, argv[3]);
+    if(argv[4] != NULL) {
+        
+        std::ifstream heuristic (argv[4]);
+        if (heuristic.is_open()) {
+            while ( heuristic >> dest >> distance ) {
+                heuristic_map[dest] = distance;
+            }
+            
+            heuristic.close();
+        }
+
+        else std::cout << "Unable to open file"; 
+
+    }
+
+
+    if(argv[4] == NULL) {
+        GetUninformedPath(start_node, argv[3]);
+    } else {
+        GetInformedPath(start_node, argv[3], heuristic_map);
+    }
+
     return 0; 
 }
